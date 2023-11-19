a1967de6fbf436a463ed3365e33431ad46a69892
PCdoremiso <flowerway626@gmail.com>
2023-11-19 23:49:58 +0800

    修改打包預設路徑

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,13 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './',
   plugins: [vue()],
   optimizeDeps: {
     // exclude: ['tw-elements'], // 確保排除tw-elements，防止Vite嘗試編譯它
   },
   build: {
     // sourcemap: false,
+
   },
 })
